a7c8c16b49ec426c6fc69517070bd39103b359b8
emmanuerl <emekaemmanuel045@gmail.com>
2020-10-05 13:51:43 +0100

    refactor verify admin middleware

--- src/auditTrail/routes/index.js
+++ src/auditTrail/routes/index.js
@@ -5,7 +5,13 @@ import { verifyAdmin } from "../../roles/hospital/middlewares";
 import { verifyUser } from "../../auth/middlewares";
 
 const logRouter = Router();
-logRouter.use(verifyUser, verifyAdmin);
+//logRouter.use(verifyUser, verifyAdmin);
+logRouter.use(verifyUser);
+logRouter.get("/", (req, res) => {
+  res.json({
+    message: "welcome to the audit trail route",
+  });
+});
 logRouter.use("/staff", staffLogRouter);
 //logRouter.use("/patient", patientLogRouter);
 
