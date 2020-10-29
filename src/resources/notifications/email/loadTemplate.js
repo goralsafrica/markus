5f93d790689caebbb2ad0805cc5665c47ff18312
emmanuerl <emekaemmanuel045@gmail.com>
2020-10-29 10:19:13 +0100

    debugginh mailer

--- src/resources/notifications/email/loadTemplate.js
+++ src/resources/notifications/email/loadTemplate.js
@@ -1,14 +1,14 @@
 import { compile } from "handlebars";
 import { join } from "path";
-import { readFileSync } from "fs";
+import { readFile } from "fs";
 export default async function (template, payload) {
   try {
-    const data = readFileSync(join(__dirname, "templates/", template), "utf8");
+    console.log(join(__dirname, "templates/", template));
+    const data = readFile(join(__dirname, "templates/", template), "utf8");
     const createTemplate = compile(data);
     const mail = createTemplate(payload);
     return mail;
   } catch (err) {
-    console.log(err);
-    process.exit();
+    console.log(err.message);
   }
 }
