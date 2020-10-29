da9207211e6d04e91caa08e75378472c0645513e
emmanuerl <emekaemmanuel045@gmail.com>
2020-10-29 10:38:42 +0100

    token error handler

--- src/resources/notifications/email/loadTemplate.js
+++ src/resources/notifications/email/loadTemplate.js
@@ -1,13 +1,10 @@
 import { compile } from "handlebars";
 import { join } from "path";
-import { readFile } from "fs";
+import { readFileSync } from "fs";
 export default async function (template, payload) {
   try {
     console.log(join(__dirname, "templates/", template));
-    const data = await readFile(
-      join(__dirname, "templates/", template),
-      "utf8"
-    );
+    const data = readFileSync(join(__dirname, "templates/", template), "utf8");
     const createTemplate = compile(data);
     const mail = createTemplate(payload);
     return mail;
