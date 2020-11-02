87e2266dcf297ab80673938cf345116686a628a0
emmanuerl <emekaemmanuel045@gmail.com>
2020-11-02 12:27:25 +0100

    debugging for crdentials

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -1,9 +1,14 @@
 import socketIO from "socket.io";
+import { verifyToken } from "../utilities";
 
 export const ws_loader = (server) =>
   new Promise((resolve, reject) => {
-    const io = socketIO(server);
+    const io = socketIO(server, { origins: "*:*" });
 
+    io.use((socket, next) => {
+      console.log(socket.handshake);
+      socket.disconnect();
+    });
     io.on("connection", (socket) => {
       console.log("new socket connection", socket.id);
       console.log(socket.handshake);
