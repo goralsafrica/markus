53266e97fab9531de2cfddba7e0acfe52a05b33d
emmanuerl <emekaemmanuel045@gmail.com>
2020-11-02 16:11:25 +0100

    socket dependency refactor

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -1,33 +1,28 @@
-import socketIO from "socket.io";
 import * as handler from "../resources/notifications/in-app/wsHandlers";
 import { verifyToken } from "../utilities";
 
-export const ws_loader = (server) =>
-  new Promise((resolve) => {
-    const io = socketIO(server, { origins: "*:*" });
-
-    io.use((socket, next) => {
-      try {
-        const token = socket.handshake.query.auth_token;
-        if (!token) throw new Error("Unrecognized user");
-        const payload = verifyToken(token);
-        if (payload.temporary) throw new Error("Invalid token");
-        socket.connectedUser = payload;
-        next();
-      } catch (err) {
-        console.log(err.message);
-        socket.disconnect();
-      }
-    });
-
-    io.on("connection", (socket) => {
-      console.log("new user");
-      handler.persistSocket(socket).then(() => {
-        socket.emit("welcome", { msg: "welcome bossman !!" });
-      });
-      socket.on("disconnect", () => {
-        handler.removeSocket(socket);
-      });
-    });
-    resolve();
+export async function ws_loader(io) {
+  io.use((socket, next) => {
+    try {
+      const token = socket.handshake.query.auth_token;
+      if (!token) throw new Error("Unrecognized user");
+      const payload = verifyToken(token);
+      if (payload.temporary) throw new Error("Invalid token");
+      socket.connectedUser = payload;
+      next();
+    } catch (err) {
+      console.log(err.message);
+      socket.disconnect();
+    }
   });
+
+  io.on("connection", (socket) => {
+    console.log("new user");
+    handler.persistSocket(socket).then(() => {
+      socket.emit("welcome", { msg: "welcome bossman !!" });
+    });
+    socket.on("disconnect", () => {
+      handler.removeSocket(socket);
+    });
+  });
+}
--- src/server.js
+++ src/server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import { createServer } from "http";
+import socketIO from "socket.io";
 import loader from "./loaders";
 import * as config from "./config";
 import { ws_loader } from "./loaders/socket";
+
 const app = express();
 app.express = express;
+
+const server = createServer(app);
+const io = socketIO(server, { origins: "*:*" });
+
+async function startServer(msg) {
+  console.log(msg);
+  return ws_loader(io).then(() => {
+    server.listen(config.port, () =>
+      console.log(`server running on port ${config.port}`)
+    );
+  });
+}
+
+// Loads all configurations and routes
 loader(app, config)
-  .then((msg) => {
-    console.log(msg);
-    const server = createServer(app);
-    ws_loader(server).then(() => {
-      server.listen(config.port, () =>
-        console.log(`server running on port ${config.port}`)
-      );
-    });
-  })
+  .then(startServer)
   .catch((err) => {
     console.error(err);
     process.exit();
   });
 
+export { server, io, app };
 export default app;
