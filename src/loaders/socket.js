53abfa222987ea51dfe84ad60249bd556629b968
emmanuerl <emekaemmanuel045@gmail.com>
2020-11-02 13:08:40 +0100

    socket connect and disconnect

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -1,19 +1,33 @@
 import socketIO from "socket.io";
+import * as handler from "../resources/notifications/in-app/wsHandlers";
 import { verifyToken } from "../utilities";
 
 export const ws_loader = (server) =>
-  new Promise((resolve, reject) => {
+  new Promise((resolve) => {
     const io = socketIO(server, { origins: "*:*" });
 
     io.use((socket, next) => {
-      console.log(socket.handshake);
-      socket.disconnect();
-    });
-    io.on("connection", (socket) => {
-      console.log("new socket connection", socket.id);
-      console.log(socket.handshake);
-      socket.emit("welcome", { msg: "welcome bossman !!" });
+      try {
+        const token = socket.handshake.query.auth_token;
+        if (!token) throw new Error("Unrecognized user");
+        const payload = verifyToken(token);
+        if (payload.temporary) throw new Error("Invalid token");
+        socket.connectedUser = payload;
+        next();
+      } catch (err) {
+        console.log(err.message);
+        socket.disconnect();
+      }
     });
 
+    io.on("connection", (socket) => {
+      handler.persistSocket(socket).then(() => {
+        socket.emit("welcome", { msg: "welcome bossman !!" });
+      });
+
+      socket.on("disconnect", () => {
+        handler.removeSocket(socket);
+      });
+    });
     resolve();
   });
---
+++ src/resources/notifications/in-app/wsHandlers.js
@@ -0,0 +1,23 @@
+import TemporaryData from "../../auth/models/TemporaryData";
+
+export async function persistSocket(socket) {
+  try {
+    await TemporaryData.create({
+      staff: socket.connectedUser.staff,
+      type: "socket_connection",
+      socketID: socket.id,
+    });
+    return socket;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function removeSocket(socket) {
+  try {
+    await TemporaryData.deleteOne({ socketID: socket.id });
+    return socket;
+  } catch (err) {
+    throw err;
+  }
+}
